a90ba2d6c88dbb268e30f634b09e3ae10af199ad
Lucas Jozefovicz <lucas@kenzie.com.br>
2023-02-16 21:33:02 -0300

    Adicionados os arquivos iniciais do projeto

---
+++ script.js
@@ -0,0 +1,26 @@
+// Implemente aqui as funções
+
+
+
+
+
+function areCandidateResultsValid() {
+
+}
+
+// Chamando a função com os parametros recebidos via script de teste
+// e atribuindo a variavel que devera ser mostrada no console
+const areCandidateValid = areCandidateResultsValid(
+    process.argv[2],
+    process.argv[3],
+    process.argv[4],
+    process.argv[5],
+    process.argv[6],
+    process.argv[7],
+    process.argv[8],
+    process.argv[9],
+    process.argv[10],
+    process.argv[11],
+);
+
+// Chame aqui a função que mostra o resultado no console

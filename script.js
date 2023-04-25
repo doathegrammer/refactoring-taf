abb5dd68f18dd87bc5ac901b512d42b2bc9e2392
Eduardo <eduardohom11@gmail.com>
2023-04-25 09:57:35 -0300

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
 // Implemente aqui as funções
 
+function abs2(gender, abs) {
+    if(gender === "male" && "female"){
+        return abs >= 41
+    }
+}
 
+function swim(gender, swimDistance, swimTime, diveTime){
+if(gender === "male" && "female")
+return swimDistance >= 100 && swimTime <= 60 || diveTime >=30
+}
 
+function height2(gender, height){
+if (gender === "male"){
+return height >= 1.7
 
+ } else if (gender === "female")
+return height >= 1.6
+}
 
-function areCandidateResultsValid() {
+function bartest(gender, barReps, barSeconds){
+if(gender === "male"){
+return barReps >= 6 || barSeconds >= 15
 
+} else if(gender === "female")
+return barReps >=5 || barSeconds >= 12
+}
+
+function run(gender, runDistance, runTime){
+if(gender === "male"){
+    return runDistance >= 3000 && runTime <= 720 || runDistance >=5000 && runTime <=1200
+
+} else if(gender === "female")
+    return runDistance >= 4000 && runTime <= 900 || runDistance >= 6000 && runTime <= 1320
+}
+
+function areCandidateResultsValid(gender, height, barReps, barSeconds, abs, runDistance, runTime, swimDistance, swimTime, diveTime) {
+
+let absresult = abs2(gender, abs)
+let rswimresult = swim(gender, swimDistance, swimTime, diveTime)
+let heightresult = height2(gender, height)
+let bartestresult = bartest(gender, barReps, barSeconds)
+let runresult = run(gender, runDistance, runTime)
+
+if (absresult && rswimresult && heightresult && bartestresult && runresult) {
+    return true
+}else {
+    return false
+}
 }
 
 // Chamando a função com os parametros recebidos via script de teste
@@ -23,4 +65,10 @@ const areCandidateValid = areCandidateResultsValid(
     process.argv[11], // Tempo total de mergulho em segundos
 );
 
+
 // Chame aqui a função que mostra o resultado no console
+
+function showMessage(message) {
+    console.log(message.toString().toUpperCase());
+}
+showMessage(areCandidateValid)
